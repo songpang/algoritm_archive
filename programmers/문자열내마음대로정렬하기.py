8a190d94b2f99f1c5d2662999b9bfc297f002095
songpang <scc6920@naver.com>
2020-04-21 00:48:31 +0100

    421

--- programmers/문자열내마음대로정렬하기.py
+++ programmers/문자열내마음대로정렬하기.py
@@ -1,22 +1,48 @@
-def solution(strings, n):
-  answer = [i[n::] for i in strings]
-  preasnwer = [i[:n] for i in strings]
-  result =[0] * len(strings)
 
-  for i in range(len(answer)):
-    result[i] = hash(answer[i])
-  
-  answer.sort()
-  
-  for i in range(len(answer)):
-    for j in range(len(result)):
-      if result[j] == hash(answer[i]):
-        answer[i] = preasnwer[j]+answer[i]
-        result[j] = 0
-        break
+## 단순하게 정렬을 2번하면 해결되는 문제.
+## 한 줄로 코딩 가능.
+# def solution(strings, n):
+#     return sorted(sorted(strings), key= lambda x : x[n])
+
+def solution(strings, n):
+  firDic = {}
+  answer = []
+
+  for i in strings:
+    if i[n] not in firDic:
+      firDic[i[n]] = [i]
+    else:
+      firDic[i[n]].append(i)
+
+  for j in range(ord('a'), ord('z')+1):
+    if chr(j) in firDic.keys():
+      answer += sorted(firDic[chr(j)])
 
   return answer
 
+print(solution(['abzcd','cdzab','abzfg','abzaa','abzbb','bbzaa'], 2))
+
+# 처음 생각했다가 틀린 코드
+
+# def solution(strings, n):
+#   answer = [i[n::] for i in strings]
+#   preasnwer = [i[:n] for i in strings]
+#   result =[0] * len(strings)
+
+#   for i in range(len(answer)):
+#     result[i] = hash(answer[i])
+  
+#   answer.sort()
+  
+#   for i in range(len(answer)):
+#     for j in range(len(result)):
+#       if result[j] == hash(answer[i]):
+#         answer[i] = preasnwer[j]+answer[i]
+#         result[j] = 0
+#         break
+
+#   return answer
+
 # print(solution(['sun', 'bed', 'car'], 1))
 # print(solution(['abce', 'abcd', 'cdx'], 2))
-print(solution(['abzcd','cdzab','abzfg','abzaa','abzbb','bbzaa'], 2))
+# print(solution(['abzcd','cdzab','abzfg','abzaa','abzbb','bbzaa'], 2))
