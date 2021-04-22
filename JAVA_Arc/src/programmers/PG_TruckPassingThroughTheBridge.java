bd9075ba28d28e1afe125a4d8d1f1b8ac3bdf956
songpang <scc6920@naver.com>
2021-04-22 23:32:27 +0900

    refactor: 미해결 문제

--- JAVA_Arc/src/programmers/PG_TruckPassingThroughTheBridge.java
+++ JAVA_Arc/src/programmers/PG_TruckPassingThroughTheBridge.java
@@ -1,4 +1,37 @@
 package programmers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PG_TruckPassingThroughTheBridge {
+
+    public static void main(String[] args) {
+        System.out.println(solution(5, 5, new int[]{2, 2, 2, 2, 1, 1, 1, 1, 1}));
+    }
+
+    //미해결
+    public static int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        List<Integer> bridge = new ArrayList<>();
+        int currentWeight = 0;
+
+        for (int truck_weight : truck_weights) {
+
+            if (bridge.size() == 0) {
+                bridge.add(truck_weight);
+                currentWeight += truck_weight;
+                answer += bridge_length + 1;
+            } else if (currentWeight + truck_weight <= weight) {
+                currentWeight = currentWeight - bridge.remove(0) + truck_weight;
+                bridge.add(truck_weight);
+                answer++;
+            } else {
+                currentWeight = currentWeight - bridge.remove(0) + truck_weight;
+                bridge.add(truck_weight);
+                answer += bridge_length;
+            }
+
+        }
+        return answer;
+    }
 }
