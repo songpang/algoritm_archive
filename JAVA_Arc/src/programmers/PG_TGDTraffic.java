7101e599b66995eb46b609f10f288d0d609c11f3
songpang <scc6920@naver.com>
2021-04-29 19:24:13 +0900

    refactor: 다른 답안들 추가

--- JAVA_Arc/src/programmers/PG_TGDTraffic.java
+++ JAVA_Arc/src/programmers/PG_TGDTraffic.java
@@ -1,60 +1,57 @@
 package programmers;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PG_TGDTraffic {
 
     public static void main(String[] args) {
-        System.out.println(solution(new String[]{"2016-09-15 20:59:57.421 0.351s",
-                "2016-09-15 20:59:58.233 1.181s",
-                "2016-09-15 20:59:58.299 0.8s",
-                "2016-09-15 20:59:58.688 1.041s",
-                "2016-09-15 20:59:59.591 1.412s",
-                "2016-09-15 21:00:00.464 1.466s",
-                "2016-09-15 21:00:00.741 1.581s",
-                "2016-09-15 21:00:00.748 2.31s",
-                "2016-09-15 21:00:00.966 0.381s",
-                "2016-09-15 21:00:02.066 2.62s"}));
+
     }
 
-    public static int solution(String[] lines) {
-        int max = 1;
-        double underBound;
-        double upperBound;
+    public int solution(String[] lines) {
+        int answer = 0;
+        List<String> times = new ArrayList<>();
+        List<String> processingTime = new ArrayList<>();
 
-        int i = 0;
+        int max = 0;
+        double underBound = 0;
+        double upperBound = 0;
         for (String line : lines) {
             String[] temp = line.split(" ");
             double parseDate = parseDate(temp[1]);
-            underBound = parseDate - Double.parseDouble(temp[2].replace("s", ""));
-            upperBound = parseDate + 1;
-            i++;
 
-            int count = 1;
-            for (String line2 : lines) {
-                String[] temp2 = line2.split(" ");
-                double parseDate2 = parseDate(temp2[1]);
-                double underBound2 = parseDate2 - Double.parseDouble(temp2[2].replace("s", "")) + 0.001;
-                double upperBound2 = parseDate2 + 1;
-
-                //if 어퍼바운드 에서 1초 내에 들어온 애
-                // 언더 바운드에서 1초 내에 들어온 애
-                // 중에 큰애
-//                if () {
-//                    count++;
-//                }
-                if(underBound2 >= upperBound + 3) break;
-            }
-            if (count > max) max = count;
+//            underBound = parseDate - temp[2];
+            upperBound = parseDate + 60;
         }
-        return max;
+
+        // 1. int형 배열을 0.001초 단위로 1일치를 만들어서 비교?
+        // 2. count = 0;
+        /*
+            count = 0;
+            for()
+            times.끝시간 - pcT  = times.첫시간
+            count += 첫시간 + pcT.
+            if(count > 0) max += 1;
+
+            3. 2000개의 배열을 만들고
+            57.421 0.351
+            58.420 까지 포함
+
+            57. 071 ~ 58.420
+            2000개를 돌면서
+            2중 루프를 한 번 더도는데
+
+         */
+        // 3.
+        return answer;
     }
 
     public static double parseDate(String date) {
         String[] result = date.split(":");
         double dateToInt = Double.parseDouble(result[2])
                 + 60 * Double.parseDouble(result[1])
-                + 60 * 60 * Double.parseDouble(result[0]);
+                + 24 * 60 * Double.parseDouble(result[0]);
         return dateToInt;
     }
 }
--- JAVA_Arc/src/programmers/PG_TruckPassingThroughTheBridge.java
+++ JAVA_Arc/src/programmers/PG_TruckPassingThroughTheBridge.java
@@ -1,7 +1,6 @@
 package programmers;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class PG_TruckPassingThroughTheBridge {
 
@@ -9,33 +8,114 @@ public class PG_TruckPassingThroughTheBridge {
         System.out.println(solution(5, 5, new int[]{2, 2, 2, 2, 1, 1, 1, 1, 1}));
     }
 
-    public static int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
-        Queue<Integer> trucks = new LinkedList<>();
-        int current_weight = 0;
+//    public static int solution(int bridge_length, int weight, int[] truck_weights) {
+//        int answer = 0;
+//        Queue<Integer> trucks = new LinkedList<>();
+//        int current_weight = 0;
+//
+//        for (int truck_weight : truck_weights) {
+//            while (true) {
+//                if(trucks.isEmpty()) {
+//                    trucks.offer(truck_weight);
+//                    current_weight += truck_weight;
+//                    answer++;
+//                    break;
+//                } else if (trucks.size() == bridge_length) {
+//                    current_weight -= trucks.poll();
+//                } else {
+//                    if(current_weight + truck_weight <= weight) {
+//                        trucks.offer(truck_weight);
+//                        current_weight += truck_weight;
+//                        answer++;
+//                        break;
+//                    } else {
+//                        trucks.add(0);
+//                        answer++;
+//                    }
+//                }
+//            }
+//        }
+//        return answer + bridge_length;
+//    }
 
-        for (int truck_weight : truck_weights) {
-            while (true) {
-                if(trucks.isEmpty()) {
-                    trucks.offer(truck_weight);
-                    current_weight += truck_weight;
-                    answer++;
-                    break;
-                } else if (trucks.size() == bridge_length) {
-                    current_weight -= trucks.poll();
-                } else {
-                    if(current_weight + truck_weight <= weight) {
-                        trucks.offer(truck_weight);
-                        current_weight += truck_weight;
-                        answer++;
-                        break;
-                    } else {
-                        trucks.add(0);
-                        answer++;
-                    }
-                }
+    // 클래스를 분리한 풀이 (효율성 제한이 있으면 통과하지 못할 수도 있을듯 )
+
+    //    class Truck {
+//        int weight;
+//        int move;
+//
+//        public Truck(int weight) {
+//            this.weight = weight;
+//            this.move = 1;
+//        }
+//
+//        public void moving() {
+//            move++;
+//        }
+//    }
+//
+//    public int solution(int bridgeLength, int weight, int[] truckWeights) {
+//        Queue<Truck> waitQ = new LinkedList<>();
+//        Queue<Truck> moveQ = new LinkedList<>();
+//
+//        for (int truckWeight : truckWeights) {
+//            waitQ.offer(new Truck(truckWeight));
+//        }
+//
+//        int answer = 0;
+//        int curWeight = 0;
+//
+//        while(!waitQ.isEmpty() || !moveQ.isEmpty()) {
+//            answer++;
+//
+//            if (moveQ.isEmpty()) {
+//                Truck t = waitQ.poll();
+//                curWeight += t.weight;
+//                moveQ.offer(t);
+//                continue;
+//            }
+//
+//            for (Truck truck : moveQ) {
+//                truck.moving();
+//            }
+//
+//            if(moveQ.peek().move > bridgeLength) {
+//                Truck t = moveQ.poll();
+//                curWeight -= t.weight;
+//            }
+//
+//            if(!waitQ.isEmpty() && curWeight + waitQ.peek().weight <= weight) {
+//                Truck t = waitQ.poll();
+//                curWeight += t.weight;
+//                moveQ.offer(t);
+//            }
+//        }
+//        return answer;
+//    }
+    public static int solution(int bridge_length, int weight, int[] truck_weights) {
+        Stack<Integer> truckStack = new Stack<>();
+        Map<Integer, Integer> bridgeMap = new HashMap<>();
+
+        for(int i = truck_weights.length-1; i >= 0; i--)
+            truckStack.push(truck_weights[i]);
+
+        int answer = 0;
+        int sum = 0;
+        while(true) {
+            answer++;
+
+            if(bridgeMap.containsKey(answer))
+                bridgeMap.remove(answer);
+
+            sum = bridgeMap.values().stream().mapToInt(Number::intValue).sum();
+
+            if(!truckStack.isEmpty()) {
+                if(sum + truckStack.peek() <= weight)
+                    bridgeMap.put(answer + bridge_length, truckStack.pop());
             }
+
+            if(truckStack.isEmpty())
+               return answer + bridge_length;
         }
-        return answer + bridge_length;
     }
 }
