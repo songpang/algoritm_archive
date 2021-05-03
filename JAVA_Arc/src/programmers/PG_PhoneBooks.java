d6700ebb06a001533bb7d1507abc39b3fd184f2d
songpang <scc6920@naver.com>
2021-05-03 19:54:57 +0900

    refactor: 처음 풀었던 답안으로 업데이트

--- JAVA_Arc/src/programmers/PG_PhoneBooks.java
+++ JAVA_Arc/src/programmers/PG_PhoneBooks.java
@@ -15,8 +15,7 @@ public class PG_PhoneBooks {
         Arrays.sort(phone_book);
 
         for (int i = 0; i < phone_book.length-1; i++) {
-            if(phone_book[i+1].length() >= phone_book[i].length() && phone_book[i+1].startsWith(phone_book[i])) {
-                //phone_book[i+1].startsWith(phone_book[i]) 로 줄일 수 있음.
+            if(phone_book[i+1].length() >= phone_book[i].length() && phone_book[i+1].substring(0,phone_book[i].length()).contains(phone_book[i])) {
                 return false;
             }
         }
