f1f03f2e11475d0618dbfe15ecc4e1d13ca6a278
songpang <scc6920@naver.com>
2021-05-26 18:52:15 +0900

    refactor: BiggerIsGreater Next Permutation으로 해결

--- JAVA_Arc/src/hackerRank/BiggerIsGreater.java
+++ JAVA_Arc/src/hackerRank/BiggerIsGreater.java
@@ -4,55 +4,40 @@ import java.util.Arrays;
 
 public class BiggerIsGreater {
     public static void main(String[] args) {
-        System.out.println(biggerIsGreater("aaa"));
-        // abacbzyk
+        System.out.println(biggerIsGreater("a"));
     }
 
     public static String biggerIsGreater(String w) {
-        int[] priorityArr = new int[w.length()];
-
-        int i = 0;
-        for (char c : w.toCharArray()) {
-            priorityArr[i] = c;
-            i++;
-        }
-
         if(w.length() == 1) {
             return "no answer";
         }
+        char[] wToChar = w.toCharArray();
 
-        int count = 0;
-        Loop1:
-        for (int j = priorityArr.length - 1; j > 0; j--) {
-            for (int k = j - 1; k >= 0; k--) {
-                if (priorityArr[j] > priorityArr[k]) {
-                    int temp = priorityArr[j];
-                    priorityArr[j] = priorityArr[k];
-                    priorityArr[k] = temp;
-                    count = k;
-                    break Loop1;
+        for (int i = wToChar.length - 1; i > 0; --i) {
+            if(wToChar[i] > wToChar[i-1]) {
+                for (int j = wToChar.length - 1; j >= i ; --j) {
+                    if(wToChar[j] > wToChar[i-1]) {
+                        char temp = wToChar[j];
+                        wToChar[j] = wToChar[i - 1];
+                        wToChar[i - 1] = temp;
+                        break;
+                    }
                 }
+                int k = wToChar.length - 1;
+                while(i < k) {
+                    char temp1 = wToChar[i];
+                    wToChar[i] = wToChar[k];
+                    wToChar[k] = temp1;
+                    i++; k--;
+                }
+                break;
             }
-            if (j == 1) {
+            if(i == 1) {
                 return "no answer";
             }
         }
-
-        StringBuilder result = new StringBuilder();
-
-        int[] sortedArr = Arrays.copyOfRange(priorityArr, count + 1, priorityArr.length);
-        Arrays.sort(sortedArr);
-
-        for (int i1 : sortedArr) {
-            priorityArr[count + 1] = i1;
-            count++;
-        }
-
-        for (int i1 : priorityArr) {
-            result.append((char) i1);
-        }
-
-        return result.toString();
+        String result = new String(wToChar);
+        return result;
     }
 
     // 94347
