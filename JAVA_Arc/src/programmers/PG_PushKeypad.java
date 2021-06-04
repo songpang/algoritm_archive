7e4e672a1c47a75466517f3996e1d3fe148fa1e2
songpang <scc6920@naver.com>
2021-06-04 20:30:13 +0900

    PG_ 키패드 누르기

--- JAVA_Arc/src/programmers/PG_PushKeypad.java
+++ JAVA_Arc/src/programmers/PG_PushKeypad.java
@@ -1,6 +1,5 @@
 package programmers;
 
-import java.util.Locale;
 
 public class PG_PushKeypad {
 
@@ -10,11 +9,42 @@ public class PG_PushKeypad {
 
     public static String solution(int[] numbers, String hand) {
         StringBuilder stringBuilder = new StringBuilder();
-        hand = hand.substring(0,1).toUpperCase();
-        int locationR = 4;
-        int locationL = 4;
+        int[] left = new int[]{0, -3};
+        int[] right = new int[]{2, -3};
 
+        for (int number : numbers) {
+            if (number % 3 == 2 || number == 0) {
+                int[] temp;
+                if (number == 0) {
+                    temp = new int[]{1, -3};
+                } else {
+                    temp = new int[]{1, -(number / 3)};
+                }
+
+                double leftD = getDistance(left, temp);
+                double rightD = getDistance(right, temp);
+                if (leftD > rightD || (leftD == rightD && hand.equals("right"))) {
+                    right = temp;
+                    stringBuilder.append("R");
+                } else {
+                    left = temp;
+                    stringBuilder.append("L");
+                }
+            } else if (number % 3 == 0) {
+                stringBuilder.append("R");
+                right = new int[]{2, -(number / 3 - 1)};
+            } else {
+                stringBuilder.append("L");
+                left = new int[]{0, -(number / 3)};
+            }
+        }
 
         return stringBuilder.toString();
+    }
+
+    public static double getDistance(int[] temp, int[] xy) {
+
+        double result = Math.abs((temp[0] - xy[0])) + Math.abs((temp[1] - xy[1]));
+        return Math.sqrt(result);
     }
 }
