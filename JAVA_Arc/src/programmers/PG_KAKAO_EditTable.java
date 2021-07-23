6760c3ebc7779734cc4cf42ac57c4d4944bed59b
songpang <scc6920@naver.com>
2021-07-23 14:49:54 +0900

    refactor: solution update.

--- JAVA_Arc/src/programmers/PG_KAKAO_EditTable.java
+++ JAVA_Arc/src/programmers/PG_KAKAO_EditTable.java
@@ -9,69 +9,102 @@ public class PG_KAKAO_EditTable {
         solution(8, 2, new String[]{"D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z", "U 1", "C"});
     }
 
-
-    //배열에 담고 인덱스 움직여 탐색하는 수행 : 효율성 탈락
+    //StringBuilder 로 생각했던 것 보다 훨씬 간단하게 해결한 솔루션.
     public static String solution(int n, int k, String[] cmd) {
-        String[] result = new String[n];
-        Arrays.fill(result, "O");
-        Stack<Integer> prison = new Stack<>();
-        int currentLocation = k;
+        Stack<Integer> remove = new Stack<>();
+        int table_size = n;
 
-        for (String s : cmd) {
-            char func = s.charAt(0);
+        for(int i = 0; i < cmd.length; i++) {
+            char c = cmd[i].charAt(0);
 
-            if (s.length() > 1) {
-                int count = Integer.parseInt(s.substring(2));
-
-                if (func == 'D') {
-                    // 사이에 X가 있나 없나 검사 해줘야함.
-                    while (count != 0) {
-                        if (result[currentLocation + 1].equals("O")) {
-                            count--;
-                        }
-                        currentLocation++;
-                    }
-                } else {
-                    while (count != 0) {
-                        if (result[currentLocation - 1].equals("O")) {
-                            count--;
-                        }
-                        currentLocation--;
-                    }
-                }
+            if(c == 'U') {
+                k -= Integer.parseInt(cmd[i].substring(2));
+            } else if(c == 'D') {
+                k += Integer.parseInt(cmd[i].substring(2));
+            } else if(c == 'C') {
+                remove.push(k);
+                table_size -= 1;
+                if(k == table_size) k -= 1;
             } else {
-                if (func == 'C') {
-                    int temp = currentLocation;
-                    result[currentLocation] = "X";
-                    prison.push(currentLocation);
-
-                    if (currentLocation != n - 1) {
-                        currentLocation++;
-
-                        while (result[currentLocation].equals("X") && currentLocation != n - 1) {
-                            currentLocation++;
-                        }
-                    }
-
-                    if (result[currentLocation].equals("X") && currentLocation == n - 1) {
-                        currentLocation = temp - 1;
-                        while (result[currentLocation].equals("X") && currentLocation != 0) {
-                            currentLocation--;
-                        }
-                    }
-                } else {
-                    result[prison.pop()] = "O";
-                }
+                int r = remove.pop();
+                if(k >= r) k += 1;
+                table_size += 1;
             }
         }
 
-        StringBuilder answer = new StringBuilder();
-        for (String s : result) {
-            answer.append(s);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < table_size; i++) {
+            sb.append('O');
         }
-        return answer.toString();
+        while(!remove.isEmpty()) {
+            sb.insert(remove.pop().intValue(), 'X');
+            //스택에서 반환하는 값이 Integer 니까 intValue 로 뽑아내야함.
+        }
+        return sb.toString();
     }
 }
+
+//배열에 담고 인덱스 움직여 탐색하는 수행 : 효율성 탈락
+//    public static String solution(int n, int k, String[] cmd) {
+//        String[] result = new String[n];
+//        Arrays.fill(result, "O");
+//        Stack<Integer> prison = new Stack<>();
+//        int currentLocation = k;
+//
+//        for (String s : cmd) {
+//            char func = s.charAt(0);
+//
+//            if (s.length() > 1) {
+//                int count = Integer.parseInt(s.substring(2));
+//
+//                if (func == 'D') {
+//                    // 사이에 X가 있나 없나 검사 해줘야함.
+//                    while (count != 0) {
+//                        if (result[currentLocation + 1].equals("O")) {
+//                            count--;
+//                        }
+//                        currentLocation++;
+//                    }
+//                } else {
+//                    while (count != 0) {
+//                        if (result[currentLocation - 1].equals("O")) {
+//                            count--;
+//                        }
+//                        currentLocation--;
+//                    }
+//                }
+//            } else {
+//                if (func == 'C') {
+//                    int temp = currentLocation;
+//                    result[currentLocation] = "X";
+//                    prison.push(currentLocation);
+//
+//                    if (currentLocation != n - 1) {
+//                        currentLocation++;
+//
+//                        while (result[currentLocation].equals("X") && currentLocation != n - 1) {
+//                            currentLocation++;
+//                        }
+//                    }
+//
+//                    if (result[currentLocation].equals("X") && currentLocation == n - 1) {
+//                        currentLocation = temp - 1;
+//                        while (result[currentLocation].equals("X") && currentLocation != 0) {
+//                            currentLocation--;
+//                        }
+//                    }
+//                } else {
+//                    result[prison.pop()] = "O";
+//                }
+//            }
+//        }
+//
+//        StringBuilder answer = new StringBuilder();
+//        for (String s : result) {
+//            answer.append(s);
+//        }
+//        return answer.toString();
+//    }
 
 /*
 "U X": 현재 선택된 행에서 X칸 위에 있는 행을 선택합니다.
