9cebf03042f3ded44e8285e0dfa04f063e919599
songpang <scc6920@naver.com>
2021-07-28 23:28:49 +0900

    refactor: 백준 2615 오목 반례 찾고 클리어

--- JAVA_Arc/src/backjoon/BJ_2615.java
+++ JAVA_Arc/src/backjoon/BJ_2615.java
@@ -4,6 +4,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+// 5목 승리 시 가장 왼쪽 인덱스를 출력해야 했는데
+// 좌하로 탐색 시 가장 위의 인덱스를 출력하고 있었다...... 이걸 찾으려고 몇 시간을 고민했는가
+
 public class BJ_2615 {
     private static final int[][] plate = new int[21][21];
     private static final int[][] udrl = new int[][]{{0, 1}, {1, 1}, {1, 0}, {1, -1}};
@@ -23,6 +26,10 @@ public class BJ_2615 {
                 if (plate[i][j] != 0) {
                     for (int k = 0; k < udrl.length; k++) {
                         if (check5Stones(i, j, plate[i][j], k) == 5) {
+                            if(k == 3) {
+                                i += 4;
+                                j -= 4;
+                            }
                             System.out.println(plate[i][j]);
                             System.out.println(i + " " + j);
                             return;
